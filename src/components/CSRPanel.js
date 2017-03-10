ac08f85955de912504661f2e575566302f5cb598
Howard Ju <howard.ju@skytv.co.nz>
2017-03-10 13:43:40 +1300

    clean the lint warnings

--- src/components/CSRPanel.js
+++ src/components/CSRPanel.js
@@ -1,28 +1,28 @@
 import React, { PropTypes } from "react";
-import { Segment, Item, Label, List, Divider} from "semantic-ui-react"
+import { Segment, Item, Label, List, Divider} from "semantic-ui-react";
 
 const CSRPanel = ({csr}) => {
 
   return (
     <Segment color="blue">
-      {csr.hopServices && <Label as='a' color='orange' ribbon="right">HOP Available</Label>}
+      {csr.hopServices && <Label as="a" color="orange" ribbon="right">HOP Available</Label>}
       <Item>
         <Item.Content>
           <Item.Header as="a">{csr.name}</Item.Header>
-          <Label icon="marker" content={csr.suburb}></Label>
+          <Label icon="marker" content={csr.suburb}/>
           <Divider/>
           <Item.Description>
             <List>
               <List.Item>
-                <List.Icon name='address card' />
+                <List.Icon name="address card" />
                 <List.Content>{csr.address}</List.Content>
               </List.Item>
               <List.Item>
-                <List.Icon name='phone' />
+                <List.Icon name="phone" />
                 <List.Content>{csr.phone}</List.Content>
               </List.Item>
               <List.Item>
-                <List.Icon name='clock' />
+                <List.Icon name="clock" />
                 <List.Content>{csr.openHours}</List.Content>
               </List.Item>
             </List>
